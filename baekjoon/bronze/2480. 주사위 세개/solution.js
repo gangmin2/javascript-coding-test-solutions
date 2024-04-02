19d8adabf115e541e67f3c7ac9b48bd733f87bb7
gangmin2 <min0115b@gmail.com>
2024-04-02 12:20:44 +0900

    improve: [Bronze Ⅳ] 주사위 세개

--- baekjoon/bronze/2480. 주사위 세개/solution.js
+++ baekjoon/bronze/2480. 주사위 세개/solution.js
@@ -7,11 +7,11 @@ let award = 0;
 
 if (dice1 === dice2 && dice2 === dice3) {
     award = 10000 + dice1 * 1000;
-} else if (dice1 === dice2 && dice1 !== dice3) {
+} else if (dice1 === dice2) {
     award = 1000 + dice1 * 100;
-} else if (dice2 === dice3 && dice3 !== dice1) {
+} else if (dice2 === dice3) {
     award = 1000 + dice2 * 100;
-} else if (dice3 === dice1 && dice1 !== dice2) {
+} else if (dice3 === dice1) {
     award = 1000 + dice3 * 100;
 } else {
     award = Math.max(dice1, dice2, dice3) * 100;
