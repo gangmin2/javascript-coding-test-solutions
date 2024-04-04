7d3b6a53c20230a7a461ad11d9093e15a61dc924
gangmin2 <min0115b@gmail.com>
2024-04-05 02:08:38 +0900

    solve: [Level 0] 점의 위치 구하기

---
+++ programmers/lv0/점의 위치 구하기/solution.js
@@ -0,0 +1,11 @@
+function solution(dot) {
+    let answer = 0;
+    
+    if (dot[0] > 0) {
+        answer = (dot[1] > 0) ? 1 : 4;
+    } else {
+        answer = (dot[1] > 0) ? 2 : 3;
+    }
+    
+    return answer;
+}
