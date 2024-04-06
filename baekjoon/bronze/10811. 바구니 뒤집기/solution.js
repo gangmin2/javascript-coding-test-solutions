a2fb0fd1d94b1051a889b402671a14ee680057c8
gangmin2 <min0115b@gmail.com>
2024-04-07 07:19:50 +0900

    solve: [Bronze Ⅱ] 바구니 뒤집기 - 10811

---
+++ baekjoon/bronze/10811. 바구니 뒤집기/solution.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+
+const n = Number(input.shift().split(' ')[0]);
+let baskets = Array.from({ length: n }, (v, i) => i + 1);
+
+input.forEach(element => {
+    let [i, j] = element.split(' ').map(Number);
+    if (i !== j) {
+        i -= 1;
+    
+        let temp;
+        temp = baskets.slice(i, j);
+        temp = temp.reverse();
+        baskets.splice(i, j - i, temp);
+        baskets = baskets.flat();
+    }
+})
+
+console.log(baskets.join(' '));
+// 35m
