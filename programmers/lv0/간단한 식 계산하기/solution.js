147a6382edc6c8bd5cfb4326cfab6837321eb8d1
gangmin2 <min0115b@gmail.com>
2024-04-25 11:04:33 +0900

    solve: [Level 0] 간단한 식 계산하기

---
+++ programmers/lv0/간단한 식 계산하기/solution.js
@@ -0,0 +1,14 @@
+function solution(binomial) {
+    const [a, op, b] = binomial.split(' ');
+    
+    switch (op) {
+        case '+':
+            return Number(a) + Number(b);
+        case '-':
+            return Number(a) - Number(b);
+        case '*':
+            return Number(a) * Number(b);
+    }
+    
+    return;
+}
